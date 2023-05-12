6bde9c161d5a11a78182eb9b0d1f35985d7828ef
Arjan Hoek <arjan.hoek@topicus.nl>
2023-05-12 10:48:24 +0200

    update styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   messages: string[] = [];
-  clients: string[] = ["Giulio", "Heidi", "Florian"];
+  clients: string[] = ['Giulio', 'Heidi', 'Florian'];
 
   addMessage(message: string) {
     this.messages.push(message);
