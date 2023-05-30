b257c83dcb2a3f63c6f7763427135a7688066dbe
Arjan Hoek <132347615+ArjanHoek-Topicus@users.noreply.github.com>
2023-05-30 16:31:05 +0200

    implement user service

--- src/app/chat-page/chat-page.component.ts
+++ src/app/chat-page/chat-page.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { createMessages, createName, createNames } from 'src/mock';
 import { IMessage } from 'src/models/messageInterface';
+import { UserService } from 'src/services/user-service.service';
 
 @Component({
   selector: 'app-chat-page',
   templateUrl: './chat-page.component.html',
   styleUrls: ['./chat-page.component.scss'],
 })
-export class ChatPageComponent {
-  clients: string[] = createNames(3);
-  messages: IMessage[] = createMessages(this.clients);
-  currentUser = this.clients[0];
+export class ChatPageComponent implements OnInit {
+  clients!: string[];
+  messages!: IMessage[];
+  currentUser!: string;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.clients = this.userService.listUsers();
+    this.messages = createMessages(this.clients);
+    this.currentUser = this.clients[0];
+  }
 
   addMessage({ clientName, text, timestamp }: IMessage) {
     const message: IMessage = { clientName, text, timestamp };
@@ -18,10 +27,10 @@ export class ChatPageComponent {
   }
 
   addClient() {
-    this.clients.push(createName());
+    this.userService.addUserToChat(createName());
   }
 
   removeClient(name: string) {
-    this.clients = this.clients.filter((x) => x != name);
+    this.userService.removeUserFromChat(name);
   }
 }
--- src/services/user-service.service.ts
+++ src/services/user-service.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { createNames } from 'src/mock';
+
+const mockUsers = createNames(20);
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class UserServiceService {
+export class UserService {
+  public activeUsers: string[];
 
-  constructor() { }
+  constructor() {
+    const [user1, user2] = mockUsers;
+
+    this.activeUsers = [user1, user2];
+  }
+
+  addUserToChat(name: string) {
+    if (!this.activeUsers.includes(name)) {
+      this.activeUsers.push(name);
+    }
+  }
+
+  removeUserFromChat(name: string) {
+    const index = this.activeUsers.indexOf(name);
+    if (index !== -1) {
+      this.activeUsers.splice(index, 1);
+    }
+  }
+
+  listUsers(): string[] {
+    return this.activeUsers;
+  }
 }
